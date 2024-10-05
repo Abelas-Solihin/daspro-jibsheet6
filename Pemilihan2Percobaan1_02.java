0d62a28f077ba45d7d889850635a812f816cd223
Abelas-Solihin <abelassolihin@gmail.com>
2024-10-05 21:04:01 +0700

    modifikasi

--- Pemilihan2Percobaan1_02.java
+++ Pemilihan2Percobaan1_02.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
-    public class Pemilihan2Percobaan1_02 {
-    
-        public static void main(String[] args) {
-            Scanner input02 = new Scanner(System.in);
 
-            System.out.println("masukkan tahun");
-            int tahun = input02.nextInt();
-         
-            if (( tahun % 4 ) ==0) {
-                if ((tahun % 100) ==0) {
-                     if (tahun % 400 == 0) {
-                     System.out.println(tahun + "tahun kabisat");
-                    } else {
-                    System.out.println("Tahun Kabisat");
-                    }
+public class Pemilihan2Percobaan1_02 {
+
+    public static void main(String[] args) {
+        Scanner input02 = new Scanner(System.in);
+
+        System.out.println("Masukkan tahun:");
+        int tahun = input02.nextInt();
+
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println(tahun + " adalah tahun kabisat");
                 } else {
-                    System.out.println(" Tahun Kabisat");
+                    System.out.println(tahun + " bukan tahun kabisat");
                 }
-             } else {
-            System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println(tahun + " adalah tahun kabisat");
             }
-          } 
-
+        } else {
+            System.out.println(tahun + " bukan tahun kabisat");
         }
 
-         // System.out.println("Apakah pembayarannya menggunakan QRIS/Tunai = ");
-        // systemPembayaran = sc02.nextLine();
-    
+        input02.close();
+    }
+}
